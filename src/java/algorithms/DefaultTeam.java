b3d6b8c4dedc09ab2c21b73c723da7a2d91a65b3
saman1122 <saman11@hotmail.fr>
2018-03-19 14:41:22 +0100

    restore inputs.points + calcul dominants

--- src/java/algorithms/DefaultTeam.java
+++ src/java/algorithms/DefaultTeam.java
@@ -1,7 +1,7 @@
 package algorithms;
 
 import java.awt.Point;
-import java.util.ArrayList;
+import java.util.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,24 +10,45 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class DefaultTeam {
+
+    private HashMap<Integer,Point> saveAllPoints = new HashMap<>();
+
     public ArrayList<Point> calculDominatingSet(ArrayList<Point> points, int edgeThreshold) {
-        ArrayList<Point> result = (ArrayList<Point>)points.clone();
+        ArrayList<Point> result = new ArrayList<>();
+        ArrayList<Point> allPoints = (ArrayList<Point>)points.clone();
 
-        ArrayList<String> links = getLinks(result);
-        HashMap<Integer,Integer> linksNumber = getLinksNumber(links);
+        allPoints.forEach(x-> saveAllPoints.put(saveAllPoints.size(),x));
 
-        //Need to sort hashmap by value
+        while (allPoints.size()>0) {
+            ArrayList<String> links = getLinks(allPoints,edgeThreshold);
+            System.out.println("getLinks ok " + links.size());
 
-        //Delete the dominant and its neighbours (and add to result)
+            if (links.size() > 0) {
+                HashMap<Integer, Integer> linksNumber = getLinksNumber(links);
+                System.out.println("getLinksNumber ok " + linksNumber.size());
+                //Need to sort hashmap by value
+                Integer sommet = Collections.max(linksNumber.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
 
-        //...
+                System.out.println("max ok " + sommet);
 
-        for (int i=0;i<points.size()/3;i++) result.remove(0);
-        // if (false) result = readFromFile("output0.points");
-        // else saveToFile("output",result);
+                //Add dominant to result
+                result.add(saveAllPoints.get(sommet));
+
+                //Delete the dominant and its neighbours (and add to result)
+                allPoints.remove(saveAllPoints.get(sommet));
+                for (Integer toDelete : getNeighbours(sommet, links))
+                    allPoints.remove(saveAllPoints.get(toDelete));
+
+                System.out.println("Remain points " + allPoints.size());
+            }else {
+                allPoints.forEach(point -> result.add(point));
+                allPoints.clear();
+            }
+
+        }
 
         return result;
     }
@@ -91,8 +112,24 @@ public class DefaultTeam {
         return points;
     }
 
-    private ArrayList<String> getLinks(ArrayList<Point> input) {
+    private ArrayList<String> getLinks(ArrayList<Point> input, int edgeThreshold) {
         ArrayList<String> retour = new ArrayList<>();
+
+        for (int i = 0; i<input.size();i++) {
+            for (int j=0; j< input.size();j++) {
+                if (i != j) {
+                    Cercle c = new Cercle(input.get(i),edgeThreshold);
+                    Point compare = input.get(j);
+
+                    if (c.isInside(compare)){
+                        Integer point1 = getKeysByValue(saveAllPoints,input.get(i)).iterator().next();
+                        Integer point2 = getKeysByValue(saveAllPoints,input.get(j)).iterator().next();
+
+                        retour.add(point1 + " " + point2);
+                    }
+                }
+            }
+        }
 
         return retour;
     }
@@ -100,6 +137,55 @@ public class DefaultTeam {
     private HashMap<Integer,Integer> getLinksNumber(ArrayList<String> input) {
         HashMap<Integer,Integer> retour = new HashMap<>();
 
+        input.forEach(in -> {
+            String [] s = in.split(" ");
+            Integer nbr1 = Integer.parseInt(s[0]);
+            Integer nbr2 = Integer.parseInt(s[1]);
+
+            retour.put(nbr1,retour.getOrDefault(nbr1,0)+ 1);
+            retour.put(nbr2,retour.getOrDefault(nbr2,0)+ 1);
+        });
+
         return retour;
+    }
+
+    private ArrayList<Integer> getNeighbours(Integer position, ArrayList<String> links) {
+        ArrayList<Integer> retour = new ArrayList<>();
+
+        links.forEach(x-> {
+            String [] s = x.split(" ");
+            Integer nbr1 = Integer.parseInt(s[0]);
+            Integer nbr2 = Integer.parseInt(s[1]);
+
+            if (nbr1.equals(position)) retour.add(nbr2);
+            if (nbr2.equals(position)) retour.add(nbr1);
+
+        });
+
+        return retour;
+    }
+
+    public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        return map.entrySet()
+                .stream()
+                .filter(entry -> Objects.equals(entry.getValue(), value))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+    }
+
+    private class Cercle {
+        Point point;
+        int rayon;
+
+        public Cercle(Point point, int rayon) {
+            this.point = point;
+            this.rayon = rayon;
+        }
+
+        public boolean isInside(Point compare){
+            int d2 = (compare.x-this.point.x)*(compare.x-this.point.x) + (compare.y-this.point.y)*(compare.y-this.point.y);
+
+            return !(d2>this.rayon*this.rayon);
+        }
     }
 }
